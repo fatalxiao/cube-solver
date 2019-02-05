13b560aaed63ca39d083e3794c9b486f103651eb
liangxiaojun <fatalxiao@163.com>
2019-02-05 22:36:38 +0800

    update Test

--- test/Test.test.js
+++ test/Test.test.js
@@ -7,9 +7,10 @@ const expect = chai.expect;
 
 describe('Test', () => {
 
-    it('Basic', () =>
-        expect(CubeSolver.solve('DRLUUBFBRBLURRLRUBLRDDFDLFUFUFFDBRDUBRUFLLFDDBFLUBLRBD'))
-        .to.be.equal('D2 R\' D\' F2 B D R2 D2 R\' F2 D\' F2 U\' B2 L2 U2 D R2 U')
-    );
+    it('Basic', () => {
+        const cube = new CubeSolver();
+        cube.init('DRLUUBFBRBLURRLRUBLRDDFDLFUFUFFDBRDUBRUFLLFDDBFLUBLRBD');
+        expect(CubeSolver.solve()).to.be.equal('D2 R\' D\' F2 B D R2 D2 R\' F2 D\' F2 U\' B2 L2 U2 D R2 U');
+    });
 
 });
