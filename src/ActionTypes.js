a583009dfbc2bc5ed74d85507cba77a778af337a
liangxiaojun <fatalxiao@163.com>
2019-02-06 16:47:03 +0800

    update ActionTypes

---
+++ src/ActionTypes.js
@@ -0,0 +1,38 @@
+// Up
+export const U = 'U';
+export const U2 = 'U2';
+export const UR = 'U\'';
+export const UR2 = 'U\'2';
+
+// Front
+export const F = 'F';
+export const F2 = 'F2';
+export const FR = 'F\'';
+export const FR2 = 'F\'2';
+
+// Right
+export const R = 'R';
+export const R2 = 'R2';
+export const RR = 'R\'';
+export const RR2 = 'R\'2';
+
+// Down
+export const D = 'D';
+export const D2 = 'D2';
+export const DR = 'D\'';
+export const DR2 = 'D\'2';
+
+// Left
+export const L = 'L';
+export const L2 = 'L2';
+export const LR = 'L\'';
+export const LR2 = 'L\'2';
+
+// Back
+export const B = 'B';
+export const B2 = 'B2';
+export const BR = 'B\'';
+export const BR2 = 'B\'2';
+
+// common order
+export default [U, U2, UR, UR2, F, F2, FR, FR2, R, R2, RR, RR2, D, D2, DR, DR2, L, L2, LR, LR2, B, B2, BR, BR2];
