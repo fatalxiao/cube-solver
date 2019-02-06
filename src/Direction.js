fe04833e1f75a63055dc96584c9ed442687ebc55
liangxiaojun <fatalxiao@163.com>
2019-02-06 16:59:31 +0800

    update Direction

---
+++ src/Direction.js
@@ -0,0 +1,8 @@
+export const UP = 'UP';
+export const FRONT = 'FRONT';
+export const RIGHT = 'RIGHT';
+export const DOWN = 'DOWN';
+export const LEFT = 'LEFT';
+export const BACK = 'BACK';
+
+export default [UP, FRONT, RIGHT, DOWN, LEFT, BACK];
