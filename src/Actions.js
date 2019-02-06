3bc2953ba36d8ea050ce02014443aa7eafbaf61a
liangxiaojun <fatalxiao@163.com>
2019-02-06 17:35:03 +0800

    update Actions

--- src/Actions.js
+++ src/Actions.js
@@ -27,71 +27,126 @@ export const U3 = {
 };
 
 // Front
-export const F = {
-    name: 'F',
-    type: Directions.FRONT
+export const FRONT_FRONT_INDEXES = [0, 1, 2, 3, 4, 5, 6, 7, 8];
+export const FRONT_SIDE_INDEXES = [[9, 10, 11], [18, 19, 20], [36, 37, 38], [45, 46, 47]];
+export const F1 = {
+    name: ActionTypes.F1,
+    type: Directions.FRONT,
+    frontIndexes: FRONT_FRONT_INDEXES,
+    sideIndexes: FRONT_SIDE_INDEXES,
+    degree: 90
 };
 export const F2 = {
-    name: 'F2',
-    type: Directions.FRONT
+    name: ActionTypes.F2,
+    type: Directions.FRONT,
+    frontIndexes: FRONT_FRONT_INDEXES,
+    sideIndexes: FRONT_SIDE_INDEXES,
+    degree: 180
 };
-export const FR = {
-    name: 'F\'',
-    type: Directions.FRONT
+export const F3 = {
+    name: ActionTypes.F3,
+    type: Directions.FRONT,
+    frontIndexes: FRONT_FRONT_INDEXES,
+    sideIndexes: FRONT_SIDE_INDEXES,
+    degree: 270
 };
 
 // Right
-export const R = {
-    name: 'R',
-    type: Directions.RIGHT
+export const RIGHT_FRONT_INDEXES = [0, 1, 2, 3, 4, 5, 6, 7, 8];
+export const RIGHT_SIDE_INDEXES = [[9, 10, 11], [18, 19, 20], [36, 37, 38], [45, 46, 47]];
+export const R1 = {
+    name: ActionTypes.R1,
+    type: Directions.RIGHT,
+    frontIndexes: RIGHT_FRONT_INDEXES,
+    sideIndexes: RIGHT_SIDE_INDEXES,
+    degree: 90
 };
 export const R2 = {
-    name: 'R2',
-    type: Directions.RIGHT
+    name: ActionTypes.R2,
+    type: Directions.RIGHT,
+    frontIndexes: RIGHT_FRONT_INDEXES,
+    sideIndexes: RIGHT_SIDE_INDEXES,
+    degree: 180
 };
-export const RR = {
-    name: 'R\'',
-    type: Directions.RIGHT
+export const R3 = {
+    name: ActionTypes.R3,
+    type: Directions.RIGHT,
+    frontIndexes: RIGHT_FRONT_INDEXES,
+    sideIndexes: RIGHT_SIDE_INDEXES,
+    degree: 270
 };
 
 // Down
-export const D = {
-    name: 'D',
-    type: Directions.DOWN
+export const DOWN_FRONT_INDEXES = [0, 1, 2, 3, 4, 5, 6, 7, 8];
+export const DOWN_SIDE_INDEXES = [[9, 10, 11], [18, 19, 20], [36, 37, 38], [45, 46, 47]];
+export const D1 = {
+    name: ActionTypes.D1,
+    type: Directions.DOWN,
+    frontIndexes: DOWN_FRONT_INDEXES,
+    sideIndexes: DOWN_SIDE_INDEXES,
+    degree: 90
 };
 export const D2 = {
-    name: 'D2',
-    type: Directions.DOWN
+    name: ActionTypes.D2,
+    type: Directions.DOWN,
+    frontIndexes: DOWN_FRONT_INDEXES,
+    sideIndexes: DOWN_SIDE_INDEXES,
+    degree: 180
 };
-export const DR = {
-    name: 'D\'',
-    type: Directions.DOWN
+export const D3 = {
+    name: ActionTypes.D3,
+    type: Directions.DOWN,
+    frontIndexes: DOWN_FRONT_INDEXES,
+    sideIndexes: DOWN_SIDE_INDEXES,
+    degree: 270
 };
 
 // Left
-export const L = {
-    name: 'L',
-    type: Directions.LEFT
+export const LEFT_FRONT_INDEXES = [0, 1, 2, 3, 4, 5, 6, 7, 8];
+export const LEFT_SIDE_INDEXES = [[9, 10, 11], [18, 19, 20], [36, 37, 38], [45, 46, 47]];
+export const L1 = {
+    name: ActionTypes.L1,
+    type: Directions.LEFT,
+    frontIndexes: LEFT_FRONT_INDEXES,
+    sideIndexes: LEFT_SIDE_INDEXES,
+    degree: 90
 };
 export const L2 = {
-    name: 'L2',
-    type: Directions.LEFT
+    name: ActionTypes.L2,
+    type: Directions.LEFT,
+    frontIndexes: LEFT_FRONT_INDEXES,
+    sideIndexes: LEFT_SIDE_INDEXES,
+    degree: 180
 };
-export const LR = {
-    name: 'L\'',
-    type: Directions.LEFT
+export const L3 = {
+    name: ActionTypes.L3,
+    type: Directions.LEFT,
+    frontIndexes: LEFT_FRONT_INDEXES,
+    sideIndexes: LEFT_SIDE_INDEXES,
+    degree: 270
 };
 
 // Back
-export const B = {
-    name: 'B',
-    type: Directions.BACK
+export const BACK_FRONT_INDEXES = [0, 1, 2, 3, 4, 5, 6, 7, 8];
+export const BACK_SIDE_INDEXES = [[9, 10, 11], [18, 19, 20], [36, 37, 38], [45, 46, 47]];
+export const B1 = {
+    name: ActionTypes.B1,
+    type: Directions.BACK,
+    frontIndexes: BACK_FRONT_INDEXES,
+    sideIndexes: BACK_SIDE_INDEXES,
+    degree: 90
 };
 export const B2 = {
-    name: 'B2',
-    type: Directions.BACK
+    name: ActionTypes.B2,
+    type: Directions.BACK,
+    frontIndexes: BACK_FRONT_INDEXES,
+    sideIndexes: BACK_SIDE_INDEXES,
+    degree: 180
 };
-export const BR = {
-    name: 'B\'',
-    type: Directions.BACK
+export const B3 = {
+    name: ActionTypes.B3,
+    type: Directions.BACK,
+    frontIndexes: BACK_FRONT_INDEXES,
+    sideIndexes: BACK_SIDE_INDEXES,
+    degree: 270
 };
