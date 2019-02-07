b301c9ecab27eb2a15d8d92fc95ea6fa4a740577
liangxiaojun <fatalxiao@163.com>
2019-02-07 15:31:32 +0800

    update IsSolved.test

---
+++ test/IsSolved.test.js
@@ -0,0 +1,20 @@
+'use strict';
+
+import chai from 'chai';
+import Cube from '../src/Cube';
+
+const expect = chai.expect;
+
+describe('IsSolved', () => {
+
+    it('IsSolved-1', () => {
+        const cube = new Cube('RLRDUFURFRRDRRDUFRFUDLFBLLBFDLLDULBUUBLFLBBUDBFBRBDFUD');
+        expect(cube.isSolved()).to.be.equal(false);
+    });
+
+    it('IsSolved-2', () => {
+        const cube = new Cube('UUUUUUUUURRRRRRRRRFFFFFFFFFDDDDDDDDDLLLLLLLLLBBBBBBBBB');
+        expect(cube.isSolved()).to.be.equal(true);
+    });
+
+});
