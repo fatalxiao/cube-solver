6f02819e4e14e617ba8c7ca7c507b1549b2612fb
liangxiaojun <fatalxiao@163.com>
2019-02-07 12:31:26 +0800

    update Cube

--- src/Cube.js
+++ src/Cube.js
@@ -4,6 +4,31 @@ class Cube {
         this.value = value;
     }
 
+    static isValid(value) {
+
+        if (!value || value.length !== 54) {
+            return false;
+        }
+
+        const colors = {
+            R: 0,
+            O: 0,
+            Y: 0,
+            G: 0,
+            B: 0,
+            W: 0
+        };
+        for (let char of value) {
+            colors[char]++;
+        }
+        if (colors.R !== 9 || colors.O !== 9 || colors.Y !== 9 || colors.G !== 9 || colors.B !== 9 || colors.W !== 9) {
+            return false;
+        }
+
+        return true;
+
+    }
+
     valueOf() {
         return this.value;
     }
@@ -19,6 +44,8 @@ class Cube {
     }
 
     isSolved() {
+
+
 
     }
 
