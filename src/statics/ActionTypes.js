1ec337de5ec204d0a2d8e113cfeaf67211b09c19
liangxiaojun <fatalxiao@163.com>
2019-02-07 14:14:47 +0800

    update ActionTypes

--- src/statics/ActionTypes.js
+++ src/statics/ActionTypes.js
@@ -3,15 +3,15 @@ export const U1 = 'U';
 export const U2 = 'U2';
 export const U3 = 'U\'';
 
-// Front
-export const F1 = 'F';
-export const F2 = 'F2';
-export const F3 = 'F\'';
-
 // Right
 export const R1 = 'R';
 export const R2 = 'R2';
 export const R3 = 'R\'';
+
+// Front
+export const F1 = 'F';
+export const F2 = 'F2';
+export const F3 = 'F\'';
 
 // Down
 export const D1 = 'D';
@@ -31,8 +31,8 @@ export const B3 = 'B\'';
 // common order
 export default [
     [U1, U2, U3],
-    [F1, F2, F3],
     [R1, R2, R3],
+    [F1, F2, F3],
     [D1, D2, D3],
     [L1, L2, L3],
     [B1, B2, B3]
