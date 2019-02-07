253c6eb081e948a2173acfd35729b39968414940
liangxiaojun <fatalxiao@163.com>
2019-02-07 13:44:33 +0800

    update Solve.test

--- test/Solve.test.js
+++ test/Solve.test.js
@@ -5,9 +5,9 @@ import CubeSolver from '../src';
 
 const expect = chai.expect;
 
-describe('Test', () => {
+describe('Solve', () => {
 
-    it('Basic', () => {
+    it('Solve-1', () => {
         const cube = new CubeSolver();
         cube.init('DRLUUBFBRBLURRLRUBLRDDFDLFUFUFFDBRDUBRUFLLFDDBFLUBLRBD');
         expect(CubeSolver.solve()).to.be.equal('D2 R\' D\' F2 B D R2 D2 R\' F2 D\' F2 U\' B2 L2 U2 D R2 U');
