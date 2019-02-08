c82d336328596be931f3dd3120f9af0658c0090c
liangxiaojun <fatalxiao@163.com>
2019-02-08 21:13:32 +0800

    update CubeSolver

--- src/CubeSolver.js
+++ src/CubeSolver.js
@@ -1,12 +1,14 @@
 'use strict';
 
 import Cube from './Cube';
+import Actions from './statics/Actions';
+import Directions from './statics/Directions';
 
 class CubeSolver {
 
     constructor() {
 
-        this.originCube = null;
+        this.originValue = null;
         this.maxDeep = 24;
 
     }
@@ -21,16 +23,57 @@ class CubeSolver {
     }
 
     init(value) {
-        this.originCube = new Cube(value);
+        this.originValue = value;
         return this;
     }
 
-    doSolve() {
+    isSolved(target, actions) {
+
+        if (!actions || !target) {
+            return false;
+        }
+
+        const cube = new Cube(this.originValue);
+        actions.forEach(action => action && cube.transform(action));
+        return cube.isSolved(target);
+
+    }
+
+    doSolve(target, actions) {
+
+        // if (actions.length === 0) {
+        //     actions.push(Actions[0]);
+        // } else if (actions.length >= this.maxDeep) {
+        //
+        //     actions.length = this.maxDeep;
+        //
+        //     const lastAction = actions[actions.length - 1];
+        //     let index = Actions.indexOf(lastAction) + 1;
+        //
+        //     if (index >= Actions.length) {
+        //
+        //     }
+        //
+        // } else {
+        //
+        //     const lastAction = actions[actions.length - 1],
+        //         directionIndex = Directions.indexOf(lastAction.type);
+        //
+        //     if (directionIndex + 1 < Directions.length) {
+        //
+        //     }
+        //
+        // }
+        //
+        // if (this.isSolved(target, actions)) {
+        //     return actions;
+        // }
+        // return doSolve(target, actions);
 
     }
 
     solve(target) {
-
+        return this.doSolve(target, []);
     }
 
 };
