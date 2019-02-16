46f75503c7c3d0db25085f690f30915c8be2877e
liangxiaojun <fatalxiao@163.com>
2019-02-16 14:44:42 +0800

    update Solve.test

--- test/Solve.test.js
+++ test/Solve.test.js
@@ -10,7 +10,7 @@ describe('Solve', () => {
     it('Solve-1', () => {
         const cube = new CubeSolver();
         cube.init('DRLUUBFBRBLURRLRUBLRDDFDLFUFUFFDBRDUBRUFLLFDDBFLUBLRBD');
-        expect(CubeSolver.solve()).to.be.equal('D2 R\' D\' F2 B D R2 D2 R\' F2 D\' F2 U\' B2 L2 U2 D R2 U');
+        expect(cube.solve()).to.be.equal('D2 R\' D\' F2 B D R2 D2 R\' F2 D\' F2 U\' B2 L2 U2 D R2 U');
     });
 
 });
